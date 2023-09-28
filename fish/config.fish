501415722e88fd6cc199e2fbc5a4999885e6c425
sgj123456 <2586743928@qq.com>
2023-09-28 21:46:36 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx fish_greeting
     set -gx EDITOR helix
+    abbr ls eza
+    abbr la eza -a
+    abbr ll eza -l
     zoxide init fish | source
     starship init fish | source
     atuin init fish | source
